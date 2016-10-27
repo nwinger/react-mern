9b6e162edaec41580a23dcfd572fa0eb2a45c1cd
Nicklas Winger <nwi@gecko.no>
2016-10-27 12:59:03 +0200

    Include scss files in index.jsx and index.html for ExtractTextWebPlugin to include via webpack

--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,7 @@ import ReactDom from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
+require('./assets/stylesheets/base.scss');
+require('./assets/stylesheets/navigation.scss');
+
 ReactDom.render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'));
