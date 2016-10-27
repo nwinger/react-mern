9a834ca7309923e38d3d65147c609f64f5dc4705
Nicklas Winger <nwi@gecko.no>
2016-10-27 13:47:11 +0200

    Additional styling

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const Navigation = () => (
-  <div>
-    <ul>
+  <div className="frame bit-1 navigation_container">
+    <h3 className="bit-40">Journey into React</h3>
+    <ul className="bit-60 nav_menu">
       <li><Link to="dashboard">Dashboard</Link></li>
       <li><Link to="login">Login</Link></li>
     </ul>
--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
 require('./assets/stylesheets/base.scss');
-require('./assets/stylesheets/navigation.scss');
 require('./assets/stylesheets/lemonade.scss');
+require('./assets/stylesheets/navigation.scss');
 
 ReactDom.render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'));
